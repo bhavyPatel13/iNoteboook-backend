cf5913ac2947d6086c43ef0d02d5ea9fcc7b1108
bhavyPatel13 <frontentdeveloper853@gmail.com>
2025-05-02 11:21:01 +0530

    fetchAllNotes renew fetchallnotes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const connectToMongo = require('./db');
 const express = require('express');
+var cors = require('cors')
 
 connectToMongo();
 const app = express()
 const port = 5000
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -15,5 +17,5 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`iNotebook backend listening on port http://localhost:${port}`)
 })
